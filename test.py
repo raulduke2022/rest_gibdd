d4ca8e8f8118cd43eb91bd334f705953c616f2c9
raulduke2022 <sergeytersahakyan@gmail.com>
2023-04-26 10:50:31 +0300

    added pool

--- test.py
+++ test.py
@@ -1,5 +1,39 @@
-import requests
+import asyncpg
+from aiohttp import web
+from aiohttp.web_app import Application
+from aiohttp.web_request import Request
+from aiohttp.web_response import Response
+from asyncpg import Record
+from asyncpg.pool import Pool
+from typing import List, Dict
+routes = web.RouteTableDef()
+DB_KEY = 'database'
+async def create_database_pool(app: Application):
+    print('Создается пул подключений.')
+    pool: Pool = await asyncpg.create_pool(host='localhost',
+    port=5432,
+    user='raulduke',
+    password='kakacoarm',
+    database='cars',
+    min_size=6,
+    max_size=6)
+    app[DB_KEY] = pool
+async def destroy_database_pool(app: Application):
+    print('Уничтожается пул подключений.')
+    pool: Pool = app[DB_KEY]
+    await pool.close()
+@routes.get('/cars')
+async def brands(request: Request) -> Response:
+    connection: Pool = request.app[DB_KEY]
+    brand_query = 'SELECT * FROM cars;'
+    results: List[Record] = await connection.fetch(brand_query)
+    result_as_dict: List[Dict] = [dict(car) for car in results]
+    return web.json_response(result_as_dict)
 
 
-r = requests.get('http://localhost:8080/time')
-print(r.text)
+
+app = web.Application()
+app.on_startup.append(create_database_pool)
+app.on_cleanup.append(destroy_database_pool)
+app.add_routes(routes)
+web.run_app(app)
